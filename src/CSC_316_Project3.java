0b678ea5df6c0e090d0ff628cd5712a87b0a3020
John Andrew S Duvall <johnandew.duvall@gmail.com>
2015-11-16 13:00:36 -0500

    Project Created

---
+++ src/CSC_316_Project3.java
@@ -0,0 +1,16 @@
+/**
+ * Author:     John Andrew S Duvall
+ * Date:       date
+ * Project:    project
+ */
+
+public class CSC_316_Project3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
