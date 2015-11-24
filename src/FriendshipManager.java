913723be44fc9de47d184399429ccd332e0fc68f
Matthew Watkins <mpwatki2@ncsu.edu>
2015-11-24 20:30:06 +0000

    Added some logic for making friends and dealing with loners

--- src/FriendshipManager.java
+++ src/FriendshipManager.java
@@ -1,3 +1,72 @@
+import java.util.*;
+
 public class FriendshipManager {
-    // TODO: Write this
+    private int _lonerCount = 0;
+    private Graph<String> _network = new Graph<String>();
+
+    public void addLoner(String name) {
+        _lonerCount++;
+    }
+
+    public void makeFriend(String name1, String name2) {
+        Node<String> person1 = getPerson(name1, true);
+        Node<String> person2 = getPerson(name2, true);
+        person1.neighbors.add(person2);
+        person2.neighbors.add(person1);
+    }
+
+    public boolean areFriends(String name1, String name2) {
+        Node<String> person1 = getPerson(name1, false);
+        for (Node<String> friend : person1.neighbors) {
+            if (friend.value == name2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean haveMutualFriend(String name1, String name2) {
+        Node<String> popularPerson = getPerson(name1, false);
+        Node<String> shyPerson = getPerson(name2, false);
+        if (shyPerson.neighbors.size() > popularPerson.neighbors.size()) {
+            Node<String> temp = shyPerson;
+            shyPerson = popularPerson;
+            popularPerson = temp;
+        }
+        for (Node<String> friend : shyPerson.neighbors) {
+            if (friend.value == popularPerson.value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<String> getRelation(String name1, String name2) {
+        // TODO: Write this
+        return null;
+    }
+
+    public int getLonerCount() {
+        return _lonerCount;
+    }
+
+    public ArrayList<String> getPopularKids() {
+        // TODO: Write this
+        return null;
+    }
+
+    // Private methods
+    private Node<String> getPerson(String name, boolean autoAllocateLoners) {
+        if (_network.contains(name)) {
+            return _network.get(name);
+        }
+
+        if (autoAllocateLoners) {
+            // Allocate the loner to the network
+            _lonerCount--;
+            return _network.add(name);
+        }
+
+        return null;
+    }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -3,15 +3,20 @@ import java.util.*;
 public class Graph<E> {
     private TreeMap<E, Node<E>> _nodes = new TreeMap<E, Node<E>>();
 
-    public Node<E> add(E val) throws Exception {
+    public Node<E> add(E val) {
         return add(new Node<E>(val));
     }
 
-    public Node<E> add(Node<E> node) throws Exception {
-        if (_nodes.containsKey(node.value)) {
-            throw new Exception(node.value + " is already in the graph");
-        }
+    public Node<E> add(Node<E> node) {
         _nodes.put(node.value, node);
         return node;
+    }
+
+    public boolean contains(E val) {
+        return _nodes.containsKey(val);
+    }
+
+    public Node<E> get(E val) {
+        return _nodes.get(val);
     }
 }
