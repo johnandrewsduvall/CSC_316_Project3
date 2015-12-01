754e00d2ccd7302036be1fafa2b37d1512ff1883
Matthew Watkins <mpwatki2@ncsu.edu>
2015-12-01 17:33:11 +0000

    Added diff tests

--- test.sh
+++ test.sh
@@ -41,27 +41,57 @@ function test {
         echo 'Executing' $filenameWithoutExt;
         java $filenameWithoutExt 'go';
     done
-    cd $BACK_SRC_FILES_DIR;
 
     echo '-------------------------------';
 
-    # echo 'Running diff tests';
-    # cd $SRC_FILES_DIR;
-    # local inputDirLen=${#INPUT_FILES_DIR};
-    # local backSrcDirLen=${#BACK_SRC_FILES_DIR};
-    # inputDirLen=$((inputDirLen+backSrcDirLen+1));
-    # for inputFileName in $BACK_SRC_FILES_DIR$INPUT_FILES_DIR/*.txt
-    # do
-    #     fileName=${inputFileName:inputDirLen};
-    #     local outputFileName=$BACK_SRC_FILES_DIR$OUTPUT_FILES_DIR'/'$fileName;
-    #     echo 'Executing against' $fileName;
-    #     java $MAIN_PROGRAM < $inputFileName > $outputFileName;
-    #     echo 'Diffing expected vs actual for:' $fileName '. Any differences are shown below:';
-    #     local expectedOutputFileName=$BACK_SRC_FILES_DIR$EXCPECTED_OUTPUT_FILES_DIR'/'$fileName;
-    #     diff -b $expectedOutputFileName $outputFileName;
-    # done
+    echo 'Running simple';
+    java SocialNetwork '../simple-graph.txt' < '../tests/inputs/simple.txt' > '../tests/outputs/simple.txt';
+    echo 'Diffing simple';
+    diff -b '../tests/expected_outputs/simple.txt' '../tests/outputs/simple.txt' > '../tests/diffs/simple.txt';
 
-    # echo '-------------------------------';
+    echo '-------------------------------';
+
+    echo 'Running b1_isfriend';
+    java SocialNetwork '../boundary-graph.txt' < '../tests/inputs/b1_isfriend.txt' > '../tests/outputs/b1_isfriend.txt';
+    echo 'Diffing b1_isfriend';
+    diff -b '../tests/expected_outputs/b1_isfriend.txt' '../tests/outputs/b1_isfriend.txt' > '../tests/diffs/b1_isfriend.txt';
+
+    echo '-------------------------------';
+
+    echo 'Running b2_mutual';
+    java SocialNetwork '../boundary-graph.txt' < '../tests/inputs/b2_mutual.txt' > '../tests/outputs/b2_mutual.txt';
+    echo 'Diffing b2_mutual';
+    diff -b '../tests/expected_outputs/b2_mutual.txt' '../tests/outputs/b2_mutual.txt' > '../tests/diffs/b2_mutual.txt';
+
+    echo '-------------------------------';
+
+    echo 'Running b3_relation';
+    java SocialNetwork '../boundary-graph.txt' < '../tests/inputs/b3_relation.txt' > '../tests/outputs/b3_relation.txt';
+    echo 'Diffing b3_relation';
+    diff -b '../tests/expected_outputs/b3_relation.txt' '../tests/outputs/b3_relation.txt' > '../tests/diffs/b3_relation.txt';
+
+    echo '-------------------------------';
+
+    echo 'Running b4_notconnected';
+    java SocialNetwork '../boundary-graph.txt' < '../tests/inputs/b4_notconnected.txt' > '../tests/outputs/b4_notconnected.txt';
+    echo 'Diffing b4_notconnected';
+    diff -b '../tests/expected_outputs/b4_notconnected.txt' '../tests/outputs/b4_notconnected.txt' > '../tests/diffs/b4_notconnected.txt';
+
+    echo '-------------------------------';
+
+    echo 'Running b5_popular';
+    java SocialNetwork '../boundary-graph.txt' < '../tests/inputs/b5_popular.txt' > '../tests/outputs/b5_popular.txt';
+    echo 'Diffing b5_popular';
+    diff -b '../tests/expected_outputs/b5_popular.txt' '../tests/outputs/b5_popular.txt' > '../tests/diffs/b5_popular.txt';
+
+    echo '-------------------------------';
+
+    echo 'Running boundary-all';
+    java SocialNetwork '../boundary-graph.txt' < '../tests/inputs/boundary-all.txt' > '../tests/outputs/boundary-all.txt';
+    echo 'Diffing boundary-all';
+    diff -b '../tests/expected_outputs/boundary-all.txt' '../tests/outputs/boundary-all.txt' > '../tests/diffs/boundary-all.txt';
+
+    echo '-------------------------------';
 
     echo 'Cleaning up';
     cd $SRC_FILES_DIR;
