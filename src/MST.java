7b98139ce75b8abf8780a8d90f9aa3f9d22174ee
Matthew Watkins <mpwatki2@ncsu.edu>
2015-12-05 20:58:35 +0000

    Fixed string equality comparison

--- src/MST.java
+++ src/MST.java
@@ -9,9 +9,30 @@ public class MST<E> {
         _to = to;
         _start = _to.head.value.key;
         this.size = _to.size;
+
+        /*
+        LinkedListIterator<Vertex<E>> frIt = _from.iterator();
+        while (frIt.hasNext()) {
+            Vertex<E> next = frIt.next();
+            if (next == null || next.key == null) {
+                System.out.print("- ");
+            } else {
+                System.out.print(next.key + " ");
+            }
+        }
+        System.out.println();
+
+        LinkedListIterator<Vertex<E>> toIt = _to.iterator();
+        while (toIt.hasNext()) {
+            System.out.print(toIt.next().key + " ");
+        }
+        System.out.println();
+        */
     }
 
     public LinkedList<E> getPath(E to) {
+        // System.out.println("Searching for path from " + _start.toString() + " to " + to.toString());
+
         LinkedList<E> path = new LinkedList<E>();
         E search = to;
         ListNode<Vertex<E>> currentTo = null;
@@ -19,16 +40,27 @@ public class MST<E> {
         do {
             currentTo = currentTo == null ? _to.tail : currentTo.previous;
             currentFr = currentFr == null ? _from.tail : currentFr.previous;
-            if (currentTo.value.key == search) {
+            if (currentFr.value == null || currentFr.value.key == null) {
+                // System.out.println("Looking at the edge from start to " + currentTo.value.key);
+            } else {
+                // System.out.println("Looking at the edge from " + currentFr.value.key + " to " + currentTo.value.key);
+            }
+
+            // System.out.println("Is " + currentTo.value.key + " = " + search + "?");
+            if (currentTo.value.key.equals(search)) {
+                // System.out.println("Yes");
                 path.prepend(search);
                 if (search == _start) {
                     return path;
                 }
                 search = currentFr.value.key;
+            } else {
+                // System.out.println("No");
             }
         } while (currentTo.hasPrevious());
 
         // Couldn't trace a full path
+        // System.out.println("Could not find a path from " + _start.toString() + " to " + to.toString());
         return null;
     }
 
